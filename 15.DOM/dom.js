4bcb5529d88e4f4c249b5ea25ef871e5fc88f519
DineshKumar H <kumardinesh.hari@gmail.com>
2024-12-26 21:02:11 -0500

    Update dom.js

--- 15.DOM/dom.js
+++ 15.DOM/dom.js
@@ -17,7 +17,7 @@ console.log(con1);
 console.log(con1[1]);
 
 
-//!     3. traget element by the selector:
+//!     3. traget element by the queryselector:
 
 let para = document.querySelector("#para1")
 console.log(para);
@@ -55,13 +55,64 @@ con4.innerText = "hello how are u"
 con4.innerHTML = "<h1>this is container4</h1> <p>i am paragraph 1 inside the container4</p>"
 
 
-let cont5 = document.querySelector(".container").innerText      
+let cont5 = document.querySelector(".container").innerHTML      
 let cons5 = document.querySelector(".container5")
 
-cons5.innerText=cont5
-console.log(cons5);
+cons5.innerHTML = cont5
 
 
+//!-------how to apply CSS from javascript
+
+cons5.style.backgroundColor="green"
+cons5.style.color="white"
 
 
+//!------- how to add and remove class.   
 
+//how to know the classes
+let con6 = document.querySelector(".con6")
+console.log(con6.classList);
+
+//how to add the class
+con6.classList.add("hello")
+console.log(con6.classList);
+
+//how to remove the class
+con6.classList.remove("dark")
+console.log(con6.classList);
+
+con6.style.borderRadius="20px"
+con6.classList.add("hi");
+
+//!     how to create element
+
+let head1=document.createElement("h1")
+
+head1.innerText = "i am heading1 from .js file"
+
+head1.classList.add("heading1")
+
+let container1 = document.querySelector(".container")
+
+
+// container1.after(head1);
+// container1.before(head1);
+// container1.append(head1);
+container1.prepend(head1);
+
+let div = document.createElement("div");
+
+div.innerText = "i am container7 from .js file"
+
+div.classList.add("container7")
+
+let con7 = document.querySelector(".con6")
+
+con7.after(div);
+
+div.innerHTML = `<h1>this is container7</h1>
+    <ul>
+        <li>dinesh</li>
+        <li>kumar</li>
+        <li>prkash</li>
+    </ul>`
